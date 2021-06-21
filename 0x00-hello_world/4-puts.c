6d66c67f9c4fbde5689eefba7572a756a3040dcf
Immaculate Musyoka <immaculatemusyoka87@gmail.com>
2021-06-21 03:27:23 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-
-
 #include <stdio.h>
 /**
  * main - using puts
- * @parameterMystr: a variable to store the value to be printed
+ * @Mystr: a variable to store the value to be printed
  (*
  * Description: this function uses the puts standard output function)?
  * Return: The main Function returns null value 
  */
-
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
