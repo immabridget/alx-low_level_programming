be3121b1d8dc4380b64ae91513f693744885b666
Immaculate Musyoka <immaculatemusyoka87@gmail.com>
2021-06-21 10:54:39 -0700

    negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n=rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -21,7 +21,7 @@ else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
-else
+else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
